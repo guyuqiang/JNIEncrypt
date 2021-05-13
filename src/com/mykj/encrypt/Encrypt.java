d09b712d16fce7202600f38e68bc469190f5c3d1
guyuqiang <1556839332@qq.com>
2021-05-13 18:00:34 +0800

    【类型】代码整理 【描述】1.初始化项目 【程序】1.

---
+++ src/com/mykj/encrypt/Encrypt.java
@@ -0,0 +1,121 @@
+
+package com.mykj.encrypt;
+
+
+import com.redxun.core.util.BeanUtil;
+
+import java.io.*;
+import java.lang.reflect.Method;
+
+/**
+ * Auther: guyuqiang  <br/>
+ * Date: 2021/5/7:17:55  <br/>
+ * Description:
+ */
+public class Encrypt extends ClassLoader {
+
+    // 装加密后的Class文件的目录名
+    private String encryptFolderName = "encrypt";
+
+    /**
+     * 加密方法
+     * @param classFilePath 需要加密的class文件路径
+     */
+    public void encryptClass(String classFilePath) {
+
+        // 为待加密的class文件创建File对象
+        File classFile = new File(classFilePath);
+        if (!classFile.exists()) {
+            System.out.println("没有获取到文件");
+            return;
+        }
+        // 加密后的文件去掉文件后缀名
+        String encryptedFile = classFile.getParent()+File.separator+classFile.getName();
+        encryptedFile = encryptedFile.substring(0,encryptedFile.indexOf("."));
+        try (
+                FileInputStream fileInputStream = new FileInputStream(classFile);
+                BufferedInputStream bis = new BufferedInputStream(fileInputStream);
+                FileOutputStream fileOutputStream = new FileOutputStream(encryptedFile);
+                BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream)
+        ) {
+
+            // 加密
+            int data;
+            while ((data = bis.read()) != -1) {
+                bos.write(data ^ 0xFF);
+            }
+            bos.flush();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    /**
+     * 解密方法
+     * @param className 需要解密的文件名
+     */
+    protected Class decryptClass(String className) {
+        String packageName=className.replace(".", "/");
+        String classPath = getClassPath();
+        // 为待加密的文件创建File对象
+        String filePath = classPath+packageName;
+        File decryptClassFile = new File(filePath);
+        if (!decryptClassFile.exists()) {
+            System.out.println("decryptClass() File:" + filePath + " not found!");
+            return null;
+        }
+        byte[] result = null;
+        BufferedInputStream bis = null;
+        ByteArrayOutputStream bos = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream(decryptClassFile));
+            bos = new ByteArrayOutputStream();
+            // 解密
+            int data;
+            while ((data = bis.read()) != -1) {
+                bos.write(data ^ 0xFF);
+            }
+            bos.flush();
+            result = bos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bis != null) {
+                    bis.close();
+                }
+                if (bos != null) {
+                    bos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Class c = this.defineClass(className,result,0,result.length);
+        Method method = null;
+        try {
+            method = BeanUtil.getMethod(c, "test", null);
+            Object object = c.newInstance();
+            Object back = method.invoke(object, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    private static String  getClassPath(){
+        String classPath = Encrypt.class.getResource("/").getPath() ;
+        if(File.separator.equals("\\")){
+            classPath=classPath.substring(1);
+        }
+        return classPath;
+    }
+
+
+    // 测试
+    public static void main(String[] args) {
+        Encrypt encrypt = new Encrypt();
+        //encrypt.encryptClass("X:\\workspace\\my\\JNIEncrypt\\target\\classes\\com\\redxun\\sys\\core\\util\\JsaasUtil.class");
+        Class c = encrypt.decryptClass("com.redxun.sys.core.util.JsaasUtil");
+    }
+}
---
+++ src/com/redxun/sys/core/util/JsaasUtil.java
@@ -0,0 +1,19 @@
+package com.redxun.sys.core.util;
+
+import com.redxun.core.json.JsonResult;
+
+/**
+ * Auther: guyuqiang  <br/>
+ * Date: 2021/5/7:17:41  <br/>
+ * Description:
+ */
+public class JsaasUtil {
+    public void test(){
+        System.out.println("===============");
+    }
+
+    public static void main(String[] args) {
+        JsaasUtil jsaasUtil = new JsaasUtil();
+        jsaasUtil.test();
+    }
+}
